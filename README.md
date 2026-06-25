bd985eb66f0f49ad2ae45c8a1e0ac58677eb7e46
Pratyansu Prateek <pratyansuprateek16@gmail.com>
2026-06-25 12:14:26 +0530

    Made some changes in code and add readme and output SS

---
+++ README.md
@@ -0,0 +1,17 @@
+# Pattern Project (Java)
+
+## 📌 Description
+This project is created using Java in IntelliJ IDEA. It contains pattern programs for practice.
+
+## 💻 How to run
+1. Open project in IntelliJ
+2. Run the `main` method
+3. Output will be shown in console
+
+## 📸 Output
+![Output Screenshot](Pattern.png)
+
+## 🛠 Tech Used
+- Java
+- IntelliJ IDEA
+- Git & GitHub
--- src/Pattern1.java
+++ src/Pattern1.java
@@ -1,12 +1,16 @@
-
+import java.util.Scanner;
 
 public class Pattern1 {
     public static void main(String[] args) {
-        for(int i=0; i<5; i++){
-            for(int j=0;j<5;j++){
+        Scanner scr = new Scanner(System.in);
+        System.out.print("Enter the size of n= ");
+        int n = scr.nextInt();
+        for(int i=0; i<n; i++){
+            for(int j=0;j<n;j++){
                 System.out.print("*");
             }
             System.out.println();
         }
     }
 }
+
